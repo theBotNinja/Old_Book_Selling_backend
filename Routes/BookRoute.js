ac234fee03908d4341d54db158008e2150d37133
devansh joshi <devansh0814dj@gmail.com>
2026-02-28 23:34:21 +0530

    upload image working now

--- Routes/BookRoute.js
+++ Routes/BookRoute.js
@@ -2,26 +2,14 @@ const express = require('express')
 const router = express.Router()
 const Booksmodel = require('../models/Booksmodel')
 const { auth, isAdmin } = require("../middleware/authMiddleware")
-const path = require("path");
-const fs = require("fs");
+const streamifier = require("streamifier");
 const multer = require("multer");
+const cloudinary = require("../config/Cloudinary");
+const storage = multer.memoryStorage();
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const uploadPath = path.join(__dirname, "..", "public");
-        // Ensure the directory exists (multer won't create it)
-        fs.mkdirSync(uploadPath, { recursive: true });
-        console.log("Saving to:", uploadPath); // 👈 DEBUG
-        cb(null, uploadPath);
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-        const ext = path.extname(file.originalname);
-        cb(null, file.fieldname + "-" + uniqueSuffix + ext);
-    }
+const upload = multer({
+    storage
 });
-
-const upload = multer({ storage });
 
 router.get('/', async (req, res) => {
     const services = await Booksmodel.find()
@@ -34,8 +22,31 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', auth, isAdmin, upload.single('image'), async (req, res) => {
     const { Name, description, price, minimumBet, maximumBet } = req.body
-    const image = req.file ? `/static/${req.file.filename}` : null;
+    let image;
     console.log(req.file)
+    try {
+        if (!req.file) {
+            return res.status(400).json({ message: "No file uploaded" });
+        }
+        const streamUpload = () => {
+            return new Promise((resolve, reject) => {
+                const stream = cloudinary.uploader.upload_stream(
+                    { folder: "old-books" },
+                    (error, result) => {
+                        if (result) resolve(result);
+                        else reject(error);
+                    }
+                );
+
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+        const result = await streamUpload();
+        image = result.secure_url;
+    } catch (error) {
+        console.error("Upload Error:", error);
+        return res.status(500).json({ error: error.message });
+    }
     if (!Name || !price || !description || !image || !minimumBet || !maximumBet) {
         return res.status(400).send('All fields are required')
     }
